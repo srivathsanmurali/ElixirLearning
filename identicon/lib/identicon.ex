b76a02b55b9e23778db49cb2d85b50fc602f3246
Srivathsan Murali <srivathsan92@gmail.com>
2017-09-27 21:28:32 +0200

    storing data in a struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,36 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    Generate identicon based on a string.
+    -> Compute MD5
+    -> List of number based on string
+    -> Pick a color
+    -> Build grid of squares
+    -> Convert grid into image
+    -> Save image
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
+    will be the main function that is called by the user.
   """
-  def hello do
-    :world
+  def main(input) do 
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  @doc """
+    compute MD5 from string.
+  """
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+          |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+    choose the first three numbers of the hex for RGB values
+  """
+  def pick_color(%Identicon.Image{hex: [r,g,b | _rest] } = image) do
+    %Identicon.Image{image | color: {r,g,b}}
   end
 end
