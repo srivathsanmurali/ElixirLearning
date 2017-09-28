a89e79e85f0ecc6b50acc0c6b7a202aa0765c3d2
Srivathsan Murali <srivathsan92@gmail.com>
2017-09-28 22:08:00 +0200

    edit a given topic

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -22,13 +22,39 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
     
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
       
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    case Repo.get(Topic, topic_id) do
+      nil ->
+        conn
+        |> put_flash(:error, "Topic not found")
+        |> redirect(to: topic_path(conn, :index))
+      topic ->
+        changeset = Topic.changeset(topic, %{})
+        render conn, "edit.html", changeset: changeset, topic: topic
+    end
+  end
+
+  def update(conn, %{"topic" => topic, "id" => topic_id}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+    
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Edited")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
 end
