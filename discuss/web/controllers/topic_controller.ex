8daa54738c648f803656dfdd5a9fd9405ab94219
Srivathsan Murali <srivathsan92@gmail.com>
2017-09-28 21:01:38 +0200

    topics adding and listing

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,34 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+  alias Discuss.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  @doc """
+    handles new topic get request.
+
+    => Shows the new topic form
+  """
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+end
